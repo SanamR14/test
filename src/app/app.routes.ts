7698a0896384c922abd15e04454314db552bcd2e
SanamR14 <sanamrajaboopathi@gmail.com>
2024-01-14 14:55:41 +0000

    responsive page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { HomeComponent } from '../home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: LandingPageComponent},
-    {path: 'home', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'signIn', component: LandingPageComponent}
 ];
--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {FlexLayoutServerModule} from '@angular/flex-layout/server';
+
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatIconModule, FormsModule, MatFormFieldModule, MatInputModule, MatCardModule, MatGridListModule, FlexLayoutModule, FlexLayoutServerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
