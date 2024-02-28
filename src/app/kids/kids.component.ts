bd5e73a90f0ceb53e78dee19f105669f9533771a
SanamR14 <sanamrajaboopathi@gmail.com>
2024-02-28 22:40:21 +0000

    final

--- src/app/kids/kids.component.ts
+++ src/app/kids/kids.component.ts
@@ -12,9 +12,7 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
   templateUrl: './kids.component.html',
   styleUrl: './kids.component.css'
 })
-export class KidsComponent implements OnInit
-// AfterViewInit
-{
+export class KidsComponent implements OnInit, AfterViewInit{
   
    constructor(private router: Router){}
    ngOnInit() {
@@ -24,15 +22,15 @@ export class KidsComponent implements OnInit
      this.router.navigate(['/signIn']);
    }
 
-  // displayedColumns: string[] = ['category', 'type', 'size', 'cost'];
-  // dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['category', 'type', 'size', 'cost'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
-  // @ViewChild(MatPaginator)
-  // paginator!: MatPaginator;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
-  // ngAfterViewInit() {
-  //   this.dataSource.paginator = this.paginator;
-  // }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   // fetchData() : any {
   //   this.http.get('http://localhost:8080/kids').subscribe((data:any) => {
@@ -41,35 +39,35 @@ export class KidsComponent implements OnInit
   //   })
   // }
 }
-// export interface PeriodicElement {
-//   dressId: string;
-//   category: string;
-//   type: string;
-//   size: string;
-//   cost: number;
-//   count: number;
-// }
-// const ELEMENT_DATA: PeriodicElement[] = 
-// [
-//   {dressId: "ZFL 14082", category: "Shirt", type: "reversable", size: "8", cost: 14.99, count: 25},
-//   {dressId: "ZFL 14083", category: "Shirt", type: "reversable", size: "9", cost: 14.99, count: 25},
-//   {dressId: "ZFL 14084", category: "Shirt", type: "reversable", size: "10", cost: 15.49, count: 25},
-//   {dressId: "ZFL 14085", category: "Shirt", type: "reversable", size: "12", cost: 15.49, count: 30},
-//   {dressId: "ZFL 14086", category: "Shirt", type: "reversable", size: "14", cost: 16.49, count: 30},
-//   {dressId: "ZFL 14087", category: "Shirt", type: "plain", size: "8", cost: 12.99, count: 20},
-//   {dressId: "ZFL 14088", category: "Shirt", type: "plain", size: "9", cost: 12.99, count: 20},
-//   {dressId: "ZFL 14089", category: "Shirt", type: "plain", size: "10", cost: 12.99, count: 20},
-//   {dressId: "ZFL 14090", category: "Shirt", type: "plain", size: "12", cost: 12.99, count: 30},
-//   {dressId: "ZFL 14091", category: "Shirt", type: "plain", size: "14", cost: 12.99, count: 30},
-//   {dressId: "ZFL 14092", category: "Jean", type: "denim", size: "XS", cost: 24.99, count: 30},
-//   {dressId: "ZFL 14093", category: "Jean", type: "denim", size: "S", cost: 24.99, count: 30},
-//   {dressId: "ZFL 14094", category: "Jean", type: "denim", size: "M", cost: 25.99, count: 30},
-//   {dressId: "ZFL 14095", category: "Jean", type: "denim", size: "L", cost: 25.99, count: 20},
-//   {dressId: "ZFL 14096", category: "Jean", type: "denim", size: "XL", cost: 27.99, count: 10},
-//   {dressId: "ZFL 14097", category: "Shorts", type: "cotton", size: "XS", cost: 17.99, count: 30},
-//   {dressId: "ZFL 14098", category: "Shorts", type: "cotton", size: "S", cost: 17.99, count: 30},
-//   {dressId: "ZFL 14099", category: "Shorts", type: "cotton", size: "M", cost: 17.99, count: 30},
-//   {dressId: "ZFL 15000", category: "Shorts", type: "cotton", size: "L", cost: 18.99, count: 20},
-//   {dressId: "ZFL 15001", category: "Shorts", type: "cotton", size: "XL", cost: 19.99, count: 10},
+export interface PeriodicElement {
+  dressId: string;
+  category: string;
+  type: string;
+  size: string;
+  cost: number;
+  count: number;
+}
+const ELEMENT_DATA: PeriodicElement[] = 
+[
+  {dressId: "ZFL 14082", category: "Shirt", type: "reversable", size: "8", cost: 14.99, count: 25},
+  {dressId: "ZFL 14083", category: "Shirt", type: "reversable", size: "9", cost: 14.99, count: 25},
+  {dressId: "ZFL 14084", category: "Shirt", type: "reversable", size: "10", cost: 15.49, count: 25},
+  {dressId: "ZFL 14085", category: "Shirt", type: "reversable", size: "12", cost: 15.49, count: 30},
+  {dressId: "ZFL 14086", category: "Shirt", type: "reversable", size: "14", cost: 16.49, count: 30},
+  {dressId: "ZFL 14087", category: "Shirt", type: "plain", size: "8", cost: 12.99, count: 20},
+  {dressId: "ZFL 14088", category: "Shirt", type: "plain", size: "9", cost: 12.99, count: 20},
+  {dressId: "ZFL 14089", category: "Shirt", type: "plain", size: "10", cost: 12.99, count: 20},
+  {dressId: "ZFL 14090", category: "Shirt", type: "plain", size: "12", cost: 12.99, count: 30},
+  {dressId: "ZFL 14091", category: "Shirt", type: "plain", size: "14", cost: 12.99, count: 30},
+  {dressId: "ZFL 14092", category: "Jean", type: "denim", size: "XS", cost: 24.99, count: 30},
+  {dressId: "ZFL 14093", category: "Jean", type: "denim", size: "S", cost: 24.99, count: 30},
+  {dressId: "ZFL 14094", category: "Jean", type: "denim", size: "M", cost: 25.99, count: 30},
+  {dressId: "ZFL 14095", category: "Jean", type: "denim", size: "L", cost: 25.99, count: 20},
+  {dressId: "ZFL 14096", category: "Jean", type: "denim", size: "XL", cost: 27.99, count: 10},
+  {dressId: "ZFL 14097", category: "Shorts", type: "cotton", size: "XS", cost: 17.99, count: 30},
+  {dressId: "ZFL 14098", category: "Shorts", type: "cotton", size: "S", cost: 17.99, count: 30},
+  {dressId: "ZFL 14099", category: "Shorts", type: "cotton", size: "M", cost: 17.99, count: 30},
+  {dressId: "ZFL 15000", category: "Shorts", type: "cotton", size: "L", cost: 18.99, count: 20},
+  {dressId: "ZFL 15001", category: "Shorts", type: "cotton", size: "XL", cost: 19.99, count: 10},
 
-// ];
+];
