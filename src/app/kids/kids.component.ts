5e77c8fde2e320561b072df44cb5c90945092eee
SanamR14 <sanamrajaboopathi@gmail.com>
2024-02-28 21:52:26 +0000

    edit

--- src/app/kids/kids.component.ts
+++ src/app/kids/kids.component.ts
@@ -40,23 +40,16 @@ export interface PeriodicElement {
 }
 const ELEMENT_DATA: PeriodicElement[] = 
 [
-  {dressId: "ZFL 14082", category: "Shirt", type: "reversable", size: "8", cost: 14.99, count: 25},
-  {dressId: "ZFL 14084", category: "Shirt", type: "reversable", size: "10", cost: 15.49, count: 25},
   {dressId: "ZFL 14085", category: "Shirt", type: "reversable", size: "12", cost: 15.49, count: 30},
   {dressId: "ZFL 14086", category: "Shirt", type: "reversable", size: "14", cost: 16.49, count: 30},
-  {dressId: "ZFL 14087", category: "Shirt", type: "plain", size: "8", cost: 12.99, count: 20},
-  {dressId: "ZFL 14089", category: "Shirt", type: "plain", size: "10", cost: 12.99, count: 20},
   {dressId: "ZFL 14090", category: "Shirt", type: "plain", size: "12", cost: 12.99, count: 30},
   {dressId: "ZFL 14091", category: "Shirt", type: "plain", size: "14", cost: 12.99, count: 30},
-  {dressId: "ZFL 14092", category: "Jean", type: "denim", size: "XS", cost: 24.99, count: 30},
   {dressId: "ZFL 14093", category: "Jean", type: "denim", size: "S", cost: 24.99, count: 30},
   {dressId: "ZFL 14094", category: "Jean", type: "denim", size: "M", cost: 25.99, count: 30},
   {dressId: "ZFL 14095", category: "Jean", type: "denim", size: "L", cost: 25.99, count: 20},
-  {dressId: "ZFL 14096", category: "Jean", type: "denim", size: "XL", cost: 27.99, count: 10},
-  {dressId: "ZFL 14097", category: "Shorts", type: "cotton", size: "XS", cost: 17.99, count: 30},
   {dressId: "ZFL 14098", category: "Shorts", type: "cotton", size: "S", cost: 17.99, count: 30},
   {dressId: "ZFL 14099", category: "Shorts", type: "cotton", size: "M", cost: 17.99, count: 30},
   {dressId: "ZFL 15000", category: "Shorts", type: "cotton", size: "L", cost: 18.99, count: 20},
-  {dressId: "ZFL 15001", category: "Shorts", type: "cotton", size: "XL", cost: 19.99, count: 10},
+  
 
 ];
